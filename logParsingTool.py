ea574906e440540a8b6899fece101578dba4a87e
Jana Hany <92585374+codedByJana@users.noreply.github.com>
2025-08-23 23:47:09 +0300

    Add files via upload

---
+++ logParsingTool.py
@@ -0,0 +1,22 @@
+import re
+log_file = 'C:\Users\Dell\OneDrive\Documents\log_file.log'
+with open(log_file, 'r') as f:
+    #to be deleted
+    print('log_file:', log_file)
+    breakpoint()
+
+    # for(line) in f:
+    #     match = re.search(regex pattern)
+    #     if match:
+    #         timestamp = match.group(1)
+    #         log_level = match.group(2)
+    #         massege = match.group(3)
+
+    # print(f'{timestamp} _ {log_level} _ {massege}')
+    #        #to be deleted
+    #         print('match:', match)
+    #         breakpoint
+    #     #to be deleted
+    #     print('line:', line)
+    #     breakpoint()
+
